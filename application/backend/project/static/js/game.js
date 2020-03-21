3b8825b1aa3f5bc1fbc424bf957e505d98d4c46c
Erik Anderson <erik10@ksu.edu>
2020-03-21 10:03:53 -0500

    Track guesses

--- application/backend/project/static/js/game.js
+++ application/backend/project/static/js/game.js
@@ -1,3 +1,36 @@
+class GameScore {
+    constructor(cg, tg) {
+        this.correctGuesses = cg;
+        this.totalGuesses = tg;
+    }
+
+    get correctGuesses() {
+        return this._correctGuesses;
+    }
+
+    set correctGuesses(g) {
+        this._correctGuesses = g;
+    }
+
+    get totalGuesses() {
+        return this._totalGuesses;
+    }
+
+    set totalGuesses(g) {
+        this._totalGuesses = g;
+    }
+
+    incCorrect() {
+        this._correctGuesses++;
+    }
+
+    incTotal() {
+        this._totalGuesses++;
+    }
+}
+
+const GS = new GameScore(0, 0);
+
 function getQuestion() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -21,17 +54,22 @@ function checkAnswer(cid, country) {
             var correct = answer.Correct;
             if(correct == "True"){
                 console.log("Correct");
+                GS.incCorrect();
+                GS.incTotal();
                 var country = "country" + String(cid);
                 console.log(country);
                 document.getElementById(country).style['fill'] = "green";
             }
             else {
                 console.log("Incorrect");
+                GS.incTotal();
                 var incorrect_country = "country" + String(cid);
                 var correct_country = "country" + String(answer.CorrectID);
                 document.getElementById(incorrect_country).style['fill'] = "red";
                 document.getElementById(correct_country).style['fill'] = "green";
             }
+            document.getElementById("prompt-correct-count").innerHTML = GS.correctGuesses;
+            document.getElementById("prompt-total-count").innerHTML = GS.totalGuesses;
         }
     }
     var url = "/game/checkAnswer?guess=" + cid + "&country=" + country;
