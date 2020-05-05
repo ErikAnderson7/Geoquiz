3108cfd5b0ad5c2bf3205551bdf4ec6943ad5af9
Erik Anderson <erik10@ksu.edu>
2020-05-05 11:47:57 -0500

    Changed comment

--- application/multiplayer/start-server.sh
+++ application/multiplayer/start-server.sh
@@ -2,6 +2,5 @@
 
 echo "Starting Server for Multiplayer"
 
-# This gunicorn command allows for it to work with websockets. 
-# The gunicorn command I was using previously (basically the one from the normal backend) did not work with websockets. 
+# Gunicorn configured to work with websockets 
 gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:5000 multiplayer:app
